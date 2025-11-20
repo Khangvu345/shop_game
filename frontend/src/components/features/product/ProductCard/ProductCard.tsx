f1e3b3598fe0e42fe285a85cf1029efdc89cc043
Satsukki <pming1250@gmail.com>
2025-11-20 16:15:16 +0700

    #17 Done base

--- frontend/src/components/features/product/ProductCard/ProductCard.tsx
+++ frontend/src/components/features/product/ProductCard/ProductCard.tsx
@@ -1,6 +1,12 @@
+// src/components/features/product/ProductCard.tsx
+import React from 'react';
 import { Link } from 'react-router-dom';
 import type { IProduct } from '../../../../types';
-import { Button } from '../../../ui/button/Button';
+import { Button } from '../../../ui/button/Button.tsx';
+import { Card } from '../../../ui/card/Card.tsx';
+
+import './ProductCard.css'
+
 
 const formatCurrency = (amount: number) => {
     return new Intl.NumberFormat('vi-VN', {
@@ -14,22 +20,39 @@ interface ProductCardProps {
     onAddToCart: (product: IProduct) => void;
 }
 
-export const ProductCard: React.FC<ProductCardProps> = ({ product, onAddToCart }) => {
+export const ProductCard = ({ product, onAddToCart }: ProductCardProps) => {
 
     const handleAddToCartClick = () => {
         onAddToCart(product);
     };
 
     return (
-        <div>
-            <h1>${product.product_name}</h1>
-            <Button
-                onClick={handleAddToCartClick}
+        <Card className="product-card">
+            <Link to={`/products/${product.product_id}`}>
+                <img
+                    src={'../../../../assets/images/products' + product.product_id}
+                    alt={product.product_name}
+                    className="product-card-image"
+                />
+            </Link>
 
-            >
-                Thêm vào giỏ
-            </Button>
-        </div>
+            {/* Nội dung card */}
+            <div className="product-card-content">
+                <Link to={`/products/${product.product_id}`} style={{textDecoration: 'none'}}>
+                    <h3 className="product-card-title">{product.product_name}</h3>
+                </Link>
 
+                <p className="product-card-price">
+                    {formatCurrency(product.list_price)}
+                </p>
+
+                <Button
+                    onClick={handleAddToCartClick}
+                    style={{ width: '100%' }}
+                >
+                    Thêm vào giỏ
+                </Button>
+            </div>
+        </Card>
     );
 };
