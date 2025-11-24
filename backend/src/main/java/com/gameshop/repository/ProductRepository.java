e175a927235689ae1b9d7d57d5879ee197754f33
KhangVu <Khangv6002@gmail.com>
2025-11-24 21:37:17 +0700

    Sửa lỗi import

--- backend/src/main/java/com/gameshop/repository/ProductRepository.java
+++ backend/src/main/java/com/gameshop/repository/ProductRepository.java
@@ -4,7 +4,8 @@ import com.gameshop.model.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-java.util.List;
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByProductNameContaining(String keyword);
