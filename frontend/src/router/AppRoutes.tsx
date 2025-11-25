cf6f4614a0a47e1b5e9261c9f09c348a8a4f2877
Satsukki <pming1250@gmail.com>
2025-11-25 16:56:25 +0700

    #28 done

--- frontend/src/router/AppRoutes.tsx
+++ frontend/src/router/AppRoutes.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import { MainLayout } from '../components/layout/MainLayout/MainLayout';
 import { HomePage } from '../pages/user/HomePage/HomePage';
 import { ProductListPage } from '../pages/user/ProductListPage/ProductListPage';
+import {CartPage} from "../pages/user/CartPage/CartPage.tsx";
 // import { ProductDetailPage } from '../pages/ProductDetailPage'; // Sẽ thêm sau
 
 export function AppRoutes() {
@@ -11,6 +12,7 @@ export function AppRoutes() {
             <Route path="/" element={<MainLayout />}>
                 <Route index element={<HomePage />} />
                 <Route path="products" element={<ProductListPage />} />
+                <Route path="cart" element={<CartPage />} />
             </Route>
         </Routes>    );
 };
