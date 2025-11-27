e14181eda2f2ae32842b02a58c35c569ed82b8da
KhangVu <Khangv6002@gmail.com>
2025-11-28 00:24:10 +0700

    đổi tên table để khớp db

--- backend/src/main/java/com/gameshop/model/entity/Order.java
+++ backend/src/main/java/com/gameshop/model/entity/Order.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "orders") // Tên bảng là orders để tránh trùng từ khóa SQL
+@Table(name = "order") // Tên bảng là orders để tránh trùng từ khóa SQL
 public class Order {
 
     @Id
