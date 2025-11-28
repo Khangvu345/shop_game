efc2b93d1775069f6fd954868268f0fd3b7d7ef8
KhangVu <Khangv6002@gmail.com>
2025-11-28 18:51:12 +0700

    sửa lỗi duplicate trực tiếp lên dev

--- backend/src/main/java/com/gameshop/model/entity/OrderAddress.java
+++ backend/src/main/java/com/gameshop/model/entity/OrderAddress.java
@@ -43,6 +43,3 @@ public class OrderAddress {
 
     @Column(name = "postal_code", length = 20)
     private String postalCode;
-
-    @Column(name = "postal_code", length = 20)
-    private String postalCode;
