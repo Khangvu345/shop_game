2063902e71cc273eaa91175909baa42d43ae3823
Tống Việt <quangviet790@gmail.com>
2025-12-03 11:57:38 +0700

    finish footer

--- frontend/src/components/layout/MainLayout/MainLayout.tsx
+++ frontend/src/components/layout/MainLayout/MainLayout.tsx
@@ -7,6 +7,7 @@ import {Button} from "../../ui/button/Button";
 import './MainLayout.css'
 import {Logo} from "../../ui/logo/Logo.tsx";
 import {logoutUser} from "../../../store/slices/Auth/authSlice.ts";
+import Footer from '../Footer/Footer.tsx';
 
 
 
@@ -88,7 +89,10 @@ export function MainLayout() {
                 <Outlet />
             </main>
 
-            <footer className={'main-footer'}></footer>
+            <footer className={'main-footer'}>
+                <Footer />
+            </footer>
+                
 
         </div>
     );
